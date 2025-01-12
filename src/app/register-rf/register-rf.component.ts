6abac1d67c3dfc89939bffb0d681003c61548c79
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-01-12 11:03:37 +0100

    reactive forms

--- src/app/register-rf/register-rf.component.ts
+++ src/app/register-rf/register-rf.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup,  ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register-rf',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-rf.component.html',
   styleUrl: './register-rf.component.css'
 })
 export class RegisterRfComponent {
 
+  usernameCtrl: FormControl;
+  passwordCtrl: FormControl;
+
+  userForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+
+     this.usernameCtrl = fb.control("your name ...");
+     this.passwordCtrl = fb.control("your password ...");
+
+     this.userForm = fb.group({
+        username: this.usernameCtrl,
+        password: this.passwordCtrl
+    });
+  }
+  reset(): void {
+     this.usernameCtrl.setValue("");
+     this.passwordCtrl.setValue("");
+  }
+  register(): void {
+     console.log(this.userForm.value);
+     this.reset();
+  }
 }
