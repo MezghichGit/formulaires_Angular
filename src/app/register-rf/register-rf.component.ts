af8f0b35a3299c820ff86fbd89fa9caa55be33c4
Mohamed Amine MEZGHICH <git@mezghich.com>
2025-01-12 11:52:16 +0100

    les validateurs

--- src/app/register-rf/register-rf.component.ts
+++ src/app/register-rf/register-rf.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup,  ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AbstractControl, FormBuilder, FormControl, FormGroup,  ReactiveFormsModule, ValidationErrors, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register-rf',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './register-rf.component.html',
   styleUrl: './register-rf.component.css'
 })
@@ -11,17 +12,28 @@ export class RegisterRfComponent {
 
   usernameCtrl: FormControl;
   passwordCtrl: FormControl;
+  birthdateCtrl: FormControl;
 
   userForm: FormGroup;
 
+  static isOldEnough(control: AbstractControl): ValidationErrors | null {
+    // control is a date input, so we can build the Date from the value
+    const birthDatePlus18 = new Date(control.value);
+    birthDatePlus18.setFullYear(birthDatePlus18.getFullYear() + 18);
+    return birthDatePlus18 < new Date() ? null : { tooYoung: true };
+ }
+
   constructor(fb: FormBuilder) {
 
-     this.usernameCtrl = fb.control("your name ...");
-     this.passwordCtrl = fb.control("your password ...");
+     this.usernameCtrl = fb.control("",[Validators.required, Validators.minLength(3)]);
+     this.passwordCtrl = fb.control("",Validators.required);
+     this.birthdateCtrl = fb.control("", { validators: [Validators.required, RegisterRfComponent.isOldEnough], nonNullable: true 
+    });
 
      this.userForm = fb.group({
         username: this.usernameCtrl,
-        password: this.passwordCtrl
+        password: this.passwordCtrl,
+        birthdate: this.birthdateCtrl
     });
   }
   reset(): void {
